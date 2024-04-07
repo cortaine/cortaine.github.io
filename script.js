747d5c718788f68ded79f39a7e041fa008c9fdd1
Cortaine <36092300+cortaine@users.noreply.github.com>
2024-04-07 00:38:05 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
 'use strict';
 
-function openPopup(imageSrc) {
-    var popup = document.getElementById("popup");
-    var fullImage = document.getElementById("fullImage");
-    var popupContent = document.querySelector(".popup-content"); // Ensure you have this class on your popup content div
+// Define reusable variables
+var popup = document.getElementById("popup");
+var fullImage = document.getElementById("fullImage");
+var popupContent = document.querySelector(".popup-content");
 
+function openPopup(imageSrc) {
     fullImage.src = imageSrc;
     popup.style.display = "block";
-
-    // Add event listener to close the popup when clicking anywhere outside of the image
     document.addEventListener("click", outsideClickHandler);
 
-    // Close the popup when clicking on the popup content as well
-    // Ensure this doesn't interfere with other interactive elements inside your popup
-    popupContent.addEventListener('click', function (event) {
-        event.stopPropagation(); // Important to prevent this click from propagating to the document level
-        closePopup();
-    }, {
+    // Close the popup when clicking on the popup content
+    popupContent.addEventListener('click', closePopupOnce, {
         once: true
-    }); // Option to auto-remove this listener after first invocation
+    });
 }
 
 function closePopup() {
-    var popup = document.getElementById("popup");
     popup.style.display = "none";
-
-    // Remove the event listener when the popup is closed
     document.removeEventListener("click", outsideClickHandler);
 }
 
+function closePopupOnce(event) {
+    event.stopPropagation();
+    closePopup();
+}
+
 function outsideClickHandler(event) {
-    var popupContent = document.querySelector(".popup-content");
     if (!popupContent.contains(event.target)) {
         closePopup();
     }
 }
 
+var images = document.querySelectorAll('img');
+
+images.forEach(function(img) {
+    img.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
+});
+
+
+// Attach click event listeners to rectangles
 var rectangles = document.querySelectorAll('.rectangle');
 rectangles.forEach(function (rectangle) {
     rectangle.addEventListener('click', function (event) {
-        event.stopPropagation(); // Prevent propagation to the outsideClickHandler
+        event.stopPropagation();
         var imageSrc = this.querySelector('img').src;
         openPopup(imageSrc);
     });
